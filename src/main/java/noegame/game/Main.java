0df2f6d363608a63301eba29d73515a5f7f6fb94
ana.morales.gonzalez@accenture.com <ana.morales.gonzalez@accenture.com>
2023-10-11 12:00:30 +0200

    First commit.Base project.All dependencies.Create lwjgl-archetype maven

---
+++ src/main/java/noegame/game/Main.java
@@ -0,0 +1,98 @@
+package noegame.game;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFWErrorCallback.createPrint;
+import static org.lwjgl.opengl.GL.createCapabilities;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+/**
+ * @author Paul Nelson Baker
+ * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
+ * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
+ * @since 2019-05
+ * <p>
+ * Modified from <a href="https://www.lwjgl.org/guide">original example</a>
+ */
+public class Main implements AutoCloseable, Runnable {
+
+    private static final String windowTitle = "Hello, World!";
+    private static final int windowWidth = 300;
+    private static final int windowHeight = 300;
+    private long windowHandle;
+
+    public static void main(String... args) {
+        try (Main main = new Main()) {
+            main.run();
+        }
+    }
+
+    /**
+     * Convienience method that also satisfies Runnable
+     */
+    public void run() {
+        init();
+        loop();
+    }
+
+    public void init() {
+        createPrint(System.err).set();
+        System.out.println("Starting LWJGL " + Version.getVersion());
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        windowHandle = glfwCreateWindow(windowWidth, windowHeight, windowTitle, NULL, NULL);
+        if (windowHandle == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+        glfwSetKeyCallback(windowHandle, (windowHandle, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                glfwSetWindowShouldClose(windowHandle, true);
+            }
+        });
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+            glfwGetWindowSize(windowHandle, pWidth, pHeight);
+            GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            glfwSetWindowPos(
+                    windowHandle,
+                    (vidMode.width() - pWidth.get(0)) / 2,
+                    (vidMode.height() - pHeight.get(0)) / 2
+            );
+        }
+        glfwMakeContextCurrent(windowHandle);
+        glfwSwapInterval(1);
+        glfwShowWindow(windowHandle);
+        createCapabilities();
+        System.out.println("OpenGL: " + glGetString(GL_VERSION));
+        glClearColor(0.0f, 0.0f, 0.2f, 0.0f);
+    }
+
+    public void loop() {
+        while (!glfwWindowShouldClose(windowHandle)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwSwapBuffers(windowHandle);
+            glfwPollEvents();
+        }
+    }
+
+    @Override
+    public void close() {
+        glfwFreeCallbacks(windowHandle);
+        glfwDestroyWindow(windowHandle);
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+}
